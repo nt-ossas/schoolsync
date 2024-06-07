fb627dfb1f210ca9173567e5a311359e4d7288c3
Ossas <157389555+nt-ossas@users.noreply.github.com>
2024-06-07 19:31:36 +0200

    Update width.js

---
+++ width.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+            // Ottieni la larghezza dello schermo
+            var screenWidth = window.innerWidth;
+
+            // Imposta la larghezza massima del body
+            document.body.style.maxWidth = screenWidth + "px";
+        };
