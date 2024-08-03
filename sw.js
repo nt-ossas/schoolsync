468b676e194b29f49548696caf7b21eb1cf36a7a
NTOssas <157389555+EXOssas@users.noreply.github.com>
2024-08-03 21:13:39 +0200

    sw :-1:

--- sw.js
+++ sw.js
@@ -2,7 +2,8 @@ self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
             return cache.addAll([
-                "./html",
+                ".",
+                "./html/index",
                 "./html/about", 
                 "./html/materie", 
                 "./html/orario", 
