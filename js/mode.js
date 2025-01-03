bbf2b16a5308b21597c447075c922ef42b341bec
NTOssas <157389555+EXOssas@users.noreply.github.com>
2025-01-03 15:42:47 +0100

    index

--- js/mode.js
+++ js/mode.js
@@ -7,12 +7,28 @@ function applyTheme(theme) {
         root.style.setProperty('--text-color', '#E0E0FF');
         root.style.setProperty('--secondary-text-color', '#B3B3CC');
         root.style.setProperty('--nav-color', '#26264D');
+        root.style.setProperty('--bg-green', 'rgba(47, 255, 0, 0.5)');
+        root.style.setProperty('--bg-red', 'rgba(254, 25, 25, 0.5)');
+        root.style.setProperty('--bg-orange', 'rgba(255, 140, 0, 0.5)');
+        root.style.setProperty('--bg-blue', 'rgba(74, 144, 226, 0.5)');
+        root.style.setProperty('--color-green', 'rgba(47, 255, 0, 1)');
+        root.style.setProperty('--color-red', 'rgba(254, 25, 25, 1)');
+        root.style.setProperty('--color-orange', 'rgba(255, 140, 0, 1)');
+        root.style.setProperty('--color-blue', 'rgba(74, 144, 226, 1)');
     } else {
         root.style.setProperty('--bg-color', '#FFFFFF');
         root.style.setProperty('--secondary-bg-color', '#F0F0F0');
         root.style.setProperty('--text-color', '#000000');
         root.style.setProperty('--secondary-text-color', '#333333');
         root.style.setProperty('--nav-color', '#CCCCCC');
+        root.style.setProperty('--bg-green', 'rgba(47, 255, 0, 0.2)');
+        root.style.setProperty('--bg-red', 'rgba(254, 25, 25, 0.2)');
+        root.style.setProperty('--bg-orange', 'rgba(255, 140, 0, 0.2)');
+        root.style.setProperty('--bg-blue', 'rgba(74, 144, 226, 0.2)');
+        root.style.setProperty('--color-green', 'rgba(47, 255, 0, 0.8)');
+        root.style.setProperty('--color-red', 'rgba(254, 25, 25, 0.8)');
+        root.style.setProperty('--color-orange', 'rgba(255, 140, 0, 0.8)');
+        root.style.setProperty('--color-blue', 'rgba(74, 144, 226, 0.8)');
     }
 }
 
